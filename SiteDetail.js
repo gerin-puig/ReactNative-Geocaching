847f886b100c9e76d5037fd851b1290068fc3b11
Akshay Khurana <Akshaykhurana1.ak@gmail.com>
2021-08-03 11:29:18 -0400

    list can now be manipulated with distance

--- SiteDetail.js
+++ SiteDetail.js
@@ -1,13 +1,95 @@
 import React, { useEffect, useState } from 'react'
-import { View,SafeAreaView, Text, ActivityIndicator, Pressable, FlatList, Button } from 'react-native'
+import { View,SafeAreaView, Text, ActivityIndicator, Pressable, FlatList, Button, TextInput } from 'react-native'
+import MapView, { Marker } from 'react-native-maps';
+
+import { Dimensions } from 'react-native';
 
 
+const SiteDetail = ({navigation,route}) =>{
+  
+  const disItem = route.params.disItem
 
-const SiteDetail = ({navigation}) =>{
+  const [currRegion, setCurrRegion] = useState({
+    latitude:disItem.Latitude,
+    longitude: disItem.Longitude,
+    latitudeDelta: 0.005,
+    longitudeDelta:0.005
+  })
+
+  function degreesToRadians(degrees) {
+    return degrees * Math.PI / 180;
+  }
+  
+  function distanceInKmBetweenEarthCoordinates(lat1, lon1, lat2, lon2) {
+    var earthRadiusKm = 6371;
+  
+    var dLat = degreesToRadians(lat2-lat1);
+    var dLon = degreesToRadians(lon2-lon1);
+  
+    lat1 = degreesToRadians(lat1);
+    lat2 = degreesToRadians(lat2);
+  
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    return earthRadiusKm * c;
+  }
+  const [lat1, setLat1] = React.useState(null);
+  const [lon1, setLon1] = React.useState(null);
+  const [lat2, setLat2] = React.useState(null);
+  const [lon2, setLon2] = React.useState(null);
+  const [result, setResult] = React.useState(null);
+
   
   return(
-      <SafeAreaView>          
-     <Text>HELLO SITE</Text>
+      <SafeAreaView>   
+         <TextInput
+        onChangeText={setLat1}
+        value={lat1}
+        placeholder="latitude"
+        keyboardType="numeric"
+        
+      />
+      <TextInput
+        onChangeText={setLon1}
+        value={lon1}
+        placeholder="longitude"
+        keyboardType="numeric"
+      />
+        <TextInput
+        onChangeText={setLat2}
+        value={lat2}
+        placeholder="latitude"
+        keyboardType="numeric"
+        
+      />
+      <TextInput
+        onChangeText={setLon2}
+        value={lon2}
+        placeholder="longitude"
+        keyboardType="numeric"
+      />
+    <Button title = "CALULATE DIS" onPress = {()=>{
+       let result = distanceInKmBetweenEarthCoordinates(lat1,lon1,lat2,lon2)
+      
+       setResult(result)
+       }}></Button>
+
+    <Text>RESULT {result} </Text>
+           
+     <Text>{disItem.title}</Text>
+     <Text>{disItem.user}</Text>
+     <Text>{disItem.Latitude}</Text>
+     <Text>{disItem.Longitude}</Text>
+     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+     <Text>Map Screen</Text>
+     <MapView
+       style={{width:Dimensions.get("window").width, height:500}}
+       initialRegion={currRegion}
+       
+     ></MapView>
+   </View>
+
       </SafeAreaView>
   )
 }
--- list.js
+++ list.js
@@ -1,23 +1,107 @@
 import React, { useEffect, useState } from 'react'
 import { View,SafeAreaView, Text, ActivityIndicator, Pressable, FlatList, Button } from 'react-native'
 import { db } from './FirebaseManager'
+import * as Location from "expo-location"
+import Alert from "react-native-awesome-alerts";
 
 
 const list = ({navigation}) =>{
-
     const [dis,setDis] = useState([])
-    
+    let coordinates = 
+    {
+        lat: "",
+        lng:""
+    }
+    let locFlag = false
+   
     const [isLoading, setLoading] = useState(true)
 
-    const itemPressed =()=>
+    const [showAlert,setShowAlert] = useState(false)
+     const onAlert = () => {
+        setShowAlert(true)
+      }
+      const hideAlert = () =>
+      {
+          setShowAlert(false)
+      }
+    function degreesToRadians(degrees) {
+        return degrees * Math.PI / 180;
+      }
+      
+      function distanceInKmBetweenEarthCoordinates(lat1, lon1, lat2, lon2) {
+        var earthRadiusKm = 6371;
+      
+        var dLat = degreesToRadians(lat2-lat1);
+        var dLon = degreesToRadians(lon2-lon1);
+      
+        lat1 = degreesToRadians(lat1);
+        lat2 = degreesToRadians(lat2);
+      
+        var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+                Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+        return earthRadiusKm * c;
+      }
+
+    const itemPressed =(item)=>
     {
-        navigation.replace("SiteDetail")
-       
+        navigation.replace("SiteDetail",{disItem: item})
     } 
-    
+   
+    const [range,setRange] = useState(0)
+
+    const getLocationPressed = () => {
+        Location.requestForegroundPermissionsAsync().
+    then(
+      (result) => {
+        if(result.status === "granted")
+        {
+            console.log("granted")
+           return Location.getCurrentPositionAsync({})
+        }
+        else
+        {
+          console.log("blocked")
+        }
+      }
+    ).
+    then(
+      (location) => {
+        console.log(`Got the location`)
+        console.log(JSON.stringify(location))
+        locFlag = true
+        console.log("locFlag set true")
+        coordinates.lat = location.coords.latitude
+        coordinates.lng = location.coords.longitude
+        console.log(coordinates)
+      }
+    )
+    .catch((err)=>{
+          console.log("Error when requesting permission")
+          console.log(err)
+          // update the UI to let the user know what happened
+        })
+    }
+    const disMapping = () =>
+    {
+        db.collection("geocachingSites").get().then((querySnapshot) => {
+            let temp = []
+            querySnapshot.forEach((documentFromFirestore) => {
+            console.log(`${documentFromFirestore.id}, ${JSON.stringify(documentFromFirestore.data())}`)
+            let result = distanceInKmBetweenEarthCoordinates(documentFromFirestore.data().Latitude,documentFromFirestore.data().Longitude,coordinates.lat,coordinates.lng)
+            if(result<range)
+            {
+                console.log(result<range)
+                temp.push(documentFromFirestore.data())
+
+            }
+            });
+            console.log(temp)
+            setDis(temp)
+            }).finally(setLoading(false));
+    }
     const loadList = ()=>
     {
-       
         db.collection("geocachingSites").get().then((querySnapshot) => {
         let temp = []
         querySnapshot.forEach((documentFromFirestore) => {
@@ -30,13 +114,51 @@ const list = ({navigation}) =>{
     
         useEffect(loadList,[])
 
+        const incPress = () =>
+        {
+             if(range>=0)
+             {
+                let newDis = range + 10
+                setRange(newDis)
+             }
+        }
+        const decPress = () =>
+        {
+            if(range>=10)
+            {
+                let newDis = range - 10
+                setRange(newDis)
+            }
+        }
+        const  selPressed = () =>
+        {
+
+            console.log("Sel pressed")
+            console.log(range)
+            setLoading(true)
+            disMapping()
+            // if(locFlag)
+            // {
+                 
+            // }
+            // else
+            // {
+            //     onAlert()
+            // }
+        }
+            
   return(
-      <SafeAreaView>          
+      <SafeAreaView>    
+         <Button title = "+ " onPress = {incPress}></Button>
+         <Text>Range Set for {range} Kms</Text>
+         <Button title = "- " onPress = {decPress}></Button>
+         <Button title = "Select" onPress = {selPressed}></Button>
+         <Button title = "location" onPress = {getLocationPressed}></Button>
          {isLoading ? (<ActivityIndicator animating={true} size="large" />) : (
             <FlatList data={dis}
                 keyExtractor={(item, index) => { return item["title"] }}
                 renderItem={({ item, index }) => (
-                    <Pressable onPress={()=>{itemPressed(index)}} onLongPress={() => { console.log(item.title + " is selected") }}>
+                    <Pressable onPress={()=>{itemPressed(item)}} onLongPress={() => { console.log(item.title + " is selected") }}>
                         <View>
                             <Text>{item.title}</Text>
                         </View>
@@ -44,6 +166,13 @@ const list = ({navigation}) =>{
                 )}
             />
         )}
+        <Alert
+        
+        show = {showAlert}
+        onPress = {selPressed}
+        message="Need Device Location"
+          
+        />
       </SafeAreaView>
   )
 }
